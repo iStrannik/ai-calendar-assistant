23512af055f0d54665f49fe76812ed0eb79f26ad
Andrew Sitnikov <andrew.sitnikov@solidlab.ru>
2025-04-24 13:03:03 +0300

    Add pathcing for ML model

--- base_prompt.md
+++ base_prompt.md
@@ -1,4 +1,4 @@
-Suppose you have these seven functions to interact with user calendar:
+Suppose you have these four functions to interact with user calendar:
 1. `add_meeting` - adds new meeting to calendar. Has this arguments: date_begin, date_end, name of meeting, meeting description and participants, that is list of emails. date_begin and date_end is required, others is optional.
 2. `delete_meeting` - deletes meeting from calendar by name. Has this arguments: name of meeting. name parameter is required.
 3. `find_slots`- find time slots in user's calendar from and return it to user. Has this arguments: date_begin, date_end. date_begin and date_end is required.
@@ -8,7 +8,8 @@ You need to process my messages and generate appropriate function calls and text
 Separate all function calls by using three backtick symbols at the end and beginning of the function call
 Separate function calls from text to user buy using this text ======================== and newline symbol
 Don't give any explanation for function call generation
-Get current time by requesting this site `https://www.timeanddate.com/`
+Current datetime is Apr 23 15:01:38 2025, today is Wednsday
+Keep your attention at current day of week, use today's day to calculate other day's of week dates
 If user didn't provide REQUIRED arguments (and only REQUIRED) - don't generate function call and make text for user to provide all required arguments
 User can ask you to recomend him some parameters for his request - help him, in example, if user don't know where to add new meeting - you can send him information about free time slots and give recommendation about it based on this slots
 Generate as little as possible function calls for user's request
--- prompt.py
+++ prompt.py
@@ -5,6 +5,11 @@ from yandex_cloud_ml_sdk import YCloudML
 import os
 import ast
 import ast_visitor
+import time
+from datetime import datetime
+from babel.dates import format_date, format_datetime, format_time
+import json
+
 
 class MLMessager:
 
@@ -18,32 +23,34 @@ class MLMessager:
             auth=self.ML_API_KEY,
         )
         with open(prompt_filename) as f:
-            self.base_message = [{"role":"system", "text":f.read()}]
-        
+            self.format_message = f.read()
         self.function_list = ["add_meeting", "delete_meeting", "find_slots", "change_meeting"]
 
     # Here we have array of calls from parse_results
     def send_message(self, user_message):
-        current_message = self.base_message + [{"role":"user", "text":user_message}]
+        formatted_date = format_date(datetime.now(), "yyyy-MM-dd EEEE", locale='ru_RU')
+        # print(self.format_message.format(formatted_date))
+        current_message = [{"role":"system", "text":self.format_message.format(formatted_date)}] + [{"role":"user", "text":user_message}]
         result = (
-            self.sdk.models.completions("yandexgpt").configure(temperature=0.5).run(current_message)
+            self.sdk.models.completions("yandexgpt", model_version="rc").configure(temperature=0.5).run(current_message)
         )
         results = []
         for alternative in result:
-            print(alternative)
+            # print(alternative)
             results.append(self.parse_results(alternative.text))
         return results
 
     # returns pair of call list and text for user
     # You can Just use this call as it: res[1] - is Text, and iterate over res[0], i.e. function_call_name, args = res[0][0] then you just do this function_map[function_call_name](*args) 
     def parse_results(self, text):
-        splitted_parts = text.split(self.text_delimeter)
+        func_part = text[text.find(self.text_delimeter + "\n") + len(self.text_delimeter) + 1: text.rfind(self.text_delimeter + "\n")]
+        text_part = text[text.rfind(self.text_delimeter + "\n") + len(self.text_delimeter) + 1:]
         results = []
-        for texted_funcs in splitted_parts[0].split("\n"):
+        for texted_funcs in func_part.split("\n"):
             if not any([i in texted_funcs for i in self.function_list]):
                 continue
             visitor = ast_visitor.MyVisitor()
             visitor.visit(ast.parse(texted_funcs))
             results.append((visitor.function_name, visitor.args))
-        return [results, splitted_parts[1]]
+        return [results, text_part]
 
